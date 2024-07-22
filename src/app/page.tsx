f0d7d3183b67c9460f53d3f0d875fbfb372acf79
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-07-22 11:53:58 +0200

    feat: add pages with temp content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <article>
+        <h2>Välkommen Till Holmängens Koloniförening!</h2>
+        <p>
+          Mindre än två kilometer sydost om torget i Vänersborg ligger
+          Holmängens koloniområde, som förvaltas i form av intresseförening.
+        </p>
+        <p>
+          Området är nästan 45 000 kvadratmeter stort och här finns 64
+          kolonilotter av varierande storlek för sommarboende och odling.
+          Vänersborgs Stadsfullmäktige beslutade redan 1931 att arrendera ut
+          marken för koloniändamål.
+        </p>
+        <p>
+          Motivet var detsamma som i många andra städer i Sverige. Nämligen att
+          koloniträdgårdsverksamhet är av stor social betydelse och för
+          ”samhällets invånare förenad med såväl trevnad som ekonomisk fördel”.
+          Målsättningen var att kunna erbjuda en sund miljö utanför staden, för
+          rekreation och möjlighet till husbehovsodling som bidrog till
+          försörjningen 1995 bedömdes koloniområdet som en kulturhistoriskt
+          värdefull bebyggelsemiljö. Det finns fina promenadvägar både inom
+          koloniområdet och i naturen runt omkring. Ett stenkast från Vänern!
+        </p>
+        <h3>
+          <strong>Välkommen till oss!</strong>
+        </h3>
+      </article>
+    </main>
+  );
 }
