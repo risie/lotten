bfaa4cc1eb7099d2ff993dc97539226a2389f5b7
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-07-22 11:51:21 +0200

    chore: add color variables to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "complementary-color": "#7da2a9",
+        "main-bg-color": "#f7f7f7",
       },
     },
   },
