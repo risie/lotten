913c0da9efb51092665ca0b908137ca7e3b12565
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-09-02 08:16:38 +0200

    chore: fix typos in welcome text

--- src/app/Welcome.tsx
+++ src/app/Welcome.tsx
@@ -11,10 +11,10 @@ export default function Welcome() {
         <p>
           Motivet var detsamma som i många andra städer i Sverige. Nämligen att
           koloniträdgårdsverksamhet är av stor social betydelse och för
-          ”samhällets invånare föreanad med såväl trevnad som ekonomisk fördel”.
+          ”samhällets invånare förenad med såväl trevnad som ekonomisk fördel”.
           Målsättningen var att kunna erbjuda en sund miljö utanför staden, för
           rekreation och möjlighet till husbehovsodling som bidrog till
-          försörjningen 1995 bedömdes koloniområdet som en kulturhistoriskt
+          försörjningen. 1995 bedömdes koloniområdet som en kulturhistoriskt
           värdefull bebyggelsemiljö. Det finns fina promenadvägar både inom
           koloniområdet och i naturen runt omkring. Ett stenkast från Vänern!
         </p>
