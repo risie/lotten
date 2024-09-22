adb92b36d1aefed6361611c7484221d098246196
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-09-22 09:03:31 +0200

    chore: add contactSchema to validate form

---
+++ src/lib/schemas.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const emailSchema = z.string().email("Ange en giltig e-postadress");
+export const contactSchema = z.object({
+  name: z.string().nonempty("Vänligen fyll i ditt namn"),
+  email: emailSchema,
+  subject: z.string().nonempty("Ange ett ämne"),
+  message: z.string().nonempty("Skriv ett meddelande"),
+});
