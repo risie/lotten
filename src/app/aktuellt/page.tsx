fd3ee83a010faffebef1091296f8da424250eb66
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-10-12 16:33:02 +0200

    feat: add aktuellt page

---
+++ src/app/aktuellt/page.tsx
@@ -0,0 +1,15 @@
+export default function Aktuellt() {
+  return (
+    <section className="flex flex-col max-w-xl mx-auto p-3 prose">
+      <h1 className="text-2xl font-bold">Aktuellt</h1>
+      <article>
+        <h2 className="text-xl font-bold">Vattnet stängs av</h2>
+        <p>
+          Vattnet stängs av den onsdagen den 15 Oktober. Fredag den 18 Oktober
+          blåser vi ledningarna. Mer info kommer
+        </p>
+        <p>Välkommna!</p>
+      </article>
+    </section>
+  );
+}
