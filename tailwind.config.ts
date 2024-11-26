77525bae37a5671f14d67ef09c73d0a893e3b01e
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-11-26 21:54:33 +0100

    feat: use retro theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "retro"],
+    themes: ["retro"],
   },
 };
 export default config;
