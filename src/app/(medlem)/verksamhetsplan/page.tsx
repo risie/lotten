15d83779a67d5d6410afb13dae215af6969f910d
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-11-26 21:55:10 +0100

    feat: update verksamhetsplan table

--- src/app/(medlem)/verksamhetsplan/page.tsx
+++ src/app/(medlem)/verksamhetsplan/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const VerksamhetsplanPage: React.FC = () => {
+export default function VerksamhetsplanPage() {
   return (
     <div className="flex flex-col max-w-xl mx-auto p-3 prose">
       <h1 className="text-2xl font-bold">
@@ -10,83 +10,69 @@ const VerksamhetsplanPage: React.FC = () => {
         <table className="table w-full">
           <thead>
             <tr>
-              <th>Månad</th>
+              <th>Datum</th>
               <th>Aktivitet</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Januari</td>
-              <td></td>
+              <td>25 Februari </td>
+              <td>Årsmöte</td>
             </tr>
             <tr>
-              <td>Februari</td>
-              <td>25 Årsmöte</td>
+              <td>02 April </td>
+              <td>Trädgårdstunnan töms första gång i år</td>
             </tr>
             <tr>
-              <td>Mars</td>
-              <td></td>
+              <td>15 April </td>
+              <td>Vattnet sätts på</td>
             </tr>
             <tr>
-              <td>April</td>
+              <td>23 April </td>
               <td>
-                02 Trädgårdstunnan töms första gång i år
-                <br />
-                15 Vattnet sätts på
-                <br />
-                23 Föreläsning Anna Brandeby Harström, Biologisk mångfald,
-                Lokal: Syrenen
-                <br />
-                30 Sista dag för inbetalning av arrende för lotten
+                Föreläsning Anna Brandeby Harström, Biologisk mångfald, Lokal:
+                Syrenen
               </td>
             </tr>
             <tr>
-              <td>Maj</td>
-              <td>
-                04 Arbetsdag
-                <br />
-                14 Restavfallstunnan töms första gång i år
-                <br />
-                Datum ej bestämt Musikkväll, beroende på vädret
-              </td>
+              <td>30 April </td>
+              <td>Sista dag för inbetalning av arrende för lotten</td>
             </tr>
             <tr>
-              <td>Juni</td>
-              <td></td>
+              <td>04 Maj </td>
+              <td>Arbetsdag</td>
             </tr>
             <tr>
-              <td>Juli</td>
-              <td>Datum ej bestämt Sommarfest</td>
+              <td>14 Maj </td>
+              <td>Restavfallstunnan töms första gång i år</td>
             </tr>
             <tr>
-              <td>Augusti</td>
-              <td></td>
+              <td>Maj (Datum ej bestämt)</td>
+              <td>Musikkväll, beroende på vädret</td>
             </tr>
             <tr>
-              <td>September</td>
-              <td>07 Arbetsdag</td>
+              <td>Juli (Datum ej bestämt)</td>
+              <td>Sommarfest</td>
             </tr>
             <tr>
-              <td>Oktober</td>
-              <td>
-                01 Restavfallstunnan töms för sista gång i år
-                <br />
-                15 Vattnet stängs av
-              </td>
+              <td>September 07</td>
+              <td>Arbetsdag</td>
             </tr>
             <tr>
-              <td>November</td>
-              <td>05 Trädgårdstunnan töms för sista gång i år</td>
+              <td>01 Oktober </td>
+              <td>Restavfallstunnan töms för sista gång i år</td>
             </tr>
             <tr>
-              <td>December</td>
-              <td></td>
+              <td>15 Oktober </td>
+              <td>Vattnet stängs av</td>
+            </tr>
+            <tr>
+              <td>05 November </td>
+              <td>Trädgårdstunnan töms för sista gång i år</td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
   );
-};
-
-export default VerksamhetsplanPage;
+}
